3e58216bd6dced90f6a11b857804e2fbce1a2dd0
NachoCor2007 <incorrearodriguez@mail.austral.edu.ar>
2024-09-22 12:25:48 -0300

    Moved SnippetApplication to its own package.

--- src/main/java/com/services/group4/snippet/application/SnippetApplication.java
+++ src/main/java/com/services/group4/snippet/application/SnippetApplication.java
@@ -1,4 +1,4 @@
-package com.services.group4.snippet;
+package com.services.group4.snippet.application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;

b717777ece0c10a8d6d39916c42b716f68bf55e2
NachoCor2007 <incorrearodriguez@mail.austral.edu.ar>
2024-09-22 13:29:00 -0300

    Created

--- src/main/java/com/services/group4/snippet/SnippetApplication.java
+++ src/main/java/com/services/group4/snippet/SnippetApplication.java
@@ -1,9 +1,10 @@
-package com.services.group4.snippet.application;
+package com.services.group4.snippet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class SnippetApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/services/group4/snippet/controller/ParserController.java
@@ -0,0 +1,13 @@
+package com.services.group4.snippet.controller;
+
+import com.services.group4.snippet.model.CommunicationMessage;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ParserController {
+  @GetMapping("/parser/communication")
+  public CommunicationMessage testParserCommunication() {
+    return new CommunicationMessage("Snippet", "Communication between Snippet and Parser works!");
+  }
+}

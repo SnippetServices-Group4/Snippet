8092ccbca24387467da6d124cfe8d1d68c53554e
Elias85974 <128708960+Elias85974@users.noreply.github.com>
2024-10-11 12:48:42 -0300

    application can now run locally and in the container

---
+++ src/main/java/com/services/group4/snippet/DotenvConfig.java
@@ -0,0 +1,31 @@
+// src/main/java/com/services/group4/snippet/DotenvConfig.java
+package com.services.group4.snippet;
+
+import io.github.cdimascio.dotenv.Dotenv;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class DotenvConfig {
+    public static void loadEnv() {
+        if (Files.exists(Paths.get(".dockerenv"))) {
+            // Running in Docker, does not search for environment variables
+            return;
+        }
+
+        if (Files.exists(Paths.get(".env"))) {
+            Dotenv dotenv = Dotenv.load();
+            dotenv.entries().forEach(entry -> {
+                if (System.getProperty(entry.getKey()) == null) {
+                    System.setProperty(entry.getKey(), entry.getValue());
+                }
+            });
+        } else {
+            System.getenv().forEach((key, value) -> {
+                if (System.getProperty(key) == null) {
+                    System.setProperty(key, value);
+                }
+            });
+        }
+    }
+}

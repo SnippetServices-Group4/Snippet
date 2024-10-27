cbc2e8c46566e484ebaeae2efc043368bc3f3922
JuliIglesias <jiglesias@mail.austral.edu.ar>
2024-10-27 20:40:14 -0300

    SnippetRequest para convertir en JSONs

---
+++ src/main/java/com/services/group4/snippet/dto/SnippetRequest.java
@@ -0,0 +1,26 @@
+package com.services.group4.snippet.dto;
+
+public class SnippetRequest {
+  private Long userId;
+  private String title;
+  private String content;
+
+  // Constructor, getters y setters
+  public SnippetRequest() {}
+
+  public SnippetRequest(Long userId, String title, String content) {
+    this.userId = userId;
+    this.title = title;
+    this.content = content;
+  }
+
+  public Long getUserId() { return userId; }
+  public void setUserId(Long userId) { this.userId = userId; }
+
+  public String getTitle() { return title; }
+  public void setTitle(String title) { this.title = title; }
+
+  public String getContent() { return content; }
+  public void setContent(String content) { this.content = content; }
+}
+

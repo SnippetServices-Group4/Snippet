6786e01dda941f207f00c00565d8c2e0fd4cf014
JuliIglesias <jiglesias@mail.austral.edu.ar>
2024-10-27 20:35:13 -0300

    SnippetService para SnippetController convertir en Jsons

---
+++ src/main/java/com/services/group4/snippet/services/SnippetService.java
@@ -0,0 +1,16 @@
+package com.services.group4.snippet.services;
+
+import com.services.group4.snippet.dto.SnippetRequest;
+import com.services.group4.snippet.model.Snippet;
+
+public class SnippetService {
+
+  public SnippetRequest convertToDTO(Snippet snippet) {
+    return new SnippetRequest(snippet.getSnippetID(), snippet.getTitle(), snippet.getContent());
+  }
+
+  public static Snippet convertToEntity(SnippetRequest snippetDTO) {
+    return new Snippet(snippetDTO.getTitle(), snippetDTO.getContent());
+  }
+
+}

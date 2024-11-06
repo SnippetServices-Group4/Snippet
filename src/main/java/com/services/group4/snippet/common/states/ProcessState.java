0345e4ce9826add4d8ad6e6ac8acca36c43cfc65
NachoCor2007 <incorrearodriguez@mail.austral.edu.ar>
2024-11-06 13:16:06 -0300

    Spotless.

--- src/main/java/com/services/group4/snippet/common/states/ProcessState.java
+++ src/main/java/com/services/group4/snippet/common/states/ProcessState.java
@@ -1,15 +1,14 @@
 package com.services.group4.snippet.common.states;
 
 public enum ProcessState {
-    NOT_STARTED("not_started"),
-    RUNNING("running"),
-    PASSED("passed"),
-    FAILED("failed");
+  NOT_STARTED("not_started"),
+  RUNNING("running"),
+  PASSED("passed"),
+  FAILED("failed");
 
+  public final String name;
 
-    public final String name;
-
-    ProcessState(String name) {
-        this.name = name;
-    }
+  ProcessState(String name) {
+    this.name = name;
+  }
 }

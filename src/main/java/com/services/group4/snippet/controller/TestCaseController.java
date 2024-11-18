ee120b3294fc3b54304a00807dc2dab57b51b88d
NachoCor2007 <incorrearodriguez@mail.austral.edu.ar>
2024-11-18 19:21:25 -0300

    TestCaseController correction

--- src/main/java/com/services/group4/snippet/controller/TestCaseController.java
+++ src/main/java/com/services/group4/snippet/controller/TestCaseController.java
@@ -26,7 +26,7 @@ public class TestCaseController {
   }
 
   @PostMapping("/createFor/{snippetId}")
-  public ResponseEntity<ResponseDto<TestCaseResponseDto>> createSnippet(
+  public ResponseEntity<ResponseDto<TestCaseResponseDto>> createTestCase(
       @RequestBody @Valid TestCaseRequestDto testCaseRequestDto,
       @RequestHeader("userId") String userId,
       @PathVariable Long snippetId) {

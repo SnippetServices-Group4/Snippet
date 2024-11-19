dea83eeee17ba8d20c33222b54ac9b29afb3a296
JuliIglesias <jiglesias@mail.austral.edu.ar>
2024-11-19 14:51:03 -0300

    tests for snippetService el mock es medio random

--- src/test/java/com/services/group4/snippet/service/SnippetServiceTests.java
+++ src/test/java/com/services/group4/snippet/service/SnippetServiceTests.java
@@ -9,11 +9,14 @@ import com.services.group4.snippet.common.Language;
 import com.services.group4.snippet.dto.snippet.response.CompleteSnippetResponseDto;
 import com.services.group4.snippet.dto.snippet.response.ResponseDto;
 import com.services.group4.snippet.dto.snippet.response.SnippetDto;
+import com.services.group4.snippet.dto.snippet.response.SnippetResponseDto;
 import com.services.group4.snippet.model.Snippet;
 import com.services.group4.snippet.repositories.SnippetRepository;
 import com.services.group4.snippet.services.BlobStorageService;
 import com.services.group4.snippet.services.PermissionService;
 import com.services.group4.snippet.services.SnippetService;
+
+import java.util.List;
 import java.util.Optional;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -38,26 +41,24 @@ public class SnippetServiceTests {
     MockitoAnnotations.openMocks(this);
     DotenvConfig.loadEnv();
   }
-
-  //  @Test
-  //  public void testCreateSnippet() {
-  //    SnippetDto snippetDto = new SnippetDto("title", "content", "java", "1.8", ".java");
-  //    Snippet snippet = new Snippet("title", "user1", new Language("java", "1.8", ".java"));
-  //    Long snippetId = snippet.getId();
-  //
-  //    when(snippetRepository.save(any(Snippet.class))).thenReturn(snippet);
-  //    when(permissionService.grantOwnerPermission(anyLong(), anyString()))
-  //        .thenReturn(FullResponse.create("Permission granted", "permission", snippetId,
-  // HttpStatus.OK));
-  //
-  //    ResponseEntity<ResponseDto<CompleteSnippetResponseDto>> response =
-  //        snippetService.createSnippet(snippetDto, "user1", "user1");
-  //
-  //    assertNotNull(response);
-  //    assertEquals(HttpStatus.CREATED, response.getStatusCode());
-  //    assertNotNull(response.getBody());
-  //    assertEquals("Permission granted", response.getBody().message());
-  //  }
+// create snippet, no entiendo porque no mockea bien el permissionService
+//  @Test
+//  public void testCreateSnippet() {
+//    SnippetDto snippetDto = new SnippetDto("title", "content", "java", "1.8", ".java");
+//    Snippet snippet = new Snippet("title", "user1", new Language("java", "1.8", ".java"));
+//    Long snippetId = 1L;
+//
+//    when(snippetRepository.save(any(Snippet.class))).thenReturn(snippet);
+//
+//
+//    ResponseEntity<ResponseDto<CompleteSnippetResponseDto>> response =
+//        snippetService.createSnippet(snippetDto, "user1", "user1");
+//
+//    assertNotNull(response);
+//    assertEquals(HttpStatus.CREATED, response.getStatusCode());
+//    assertNotNull(response.getBody());
+//    assertEquals("Permission granted", response.getBody().message());
+//  }
 
   @Test
   public void testGetSnippet() {
@@ -82,6 +83,27 @@ public class SnippetServiceTests {
   }
 
   @Test
+  public void testGetAllSnippet() {
+      // Setup
+      Snippet snippet = new Snippet("Test Snippet", "user1", new Language("java", "1.8", ".java"));
+      List<Long> snippetIds = List.of(1L);
+
+      // Mocking
+      when(permissionService.getAllowedSnippets(anyString()))
+          .thenReturn(FullResponse.create("Permission granted", "snippetIds", snippetIds, HttpStatus.OK));
+      when(snippetRepository.findSnippetById(anyLong())).thenReturn(Optional.of(snippet));
+
+      // Test execution
+      ResponseEntity<ResponseDto<List<SnippetResponseDto>>> response = snippetService.getAllSnippet("user1");
+
+      // Assertions
+      assertEquals(HttpStatus.OK, response.getStatusCode());
+      assertNotNull(response.getBody());
+      assertEquals(1, response.getBody().data().data().size());
+      assertEquals("Test Snippet", response.getBody().data().data().get(0).name());
+  }
+
+  @Test
   public void testUpdateSnippet() {
     // Setup
     SnippetDto snippetDto = new SnippetDto(null, "Updated Content", null, null, null);
@@ -102,22 +124,23 @@ public class SnippetServiceTests {
     assertEquals("Test Snippet", response.getBody().data().data().name());
   }
 
-  //  @Test
-  //  public void testDeleteSnippet() {
-  //    // Setup
-  //    Snippet snippet = new Snippet("Test Snippet", "user1", new Language("java", "1.8",
-  // ".java"));
-  //    Long snippetId = snippet.getId();
-  //    // Mocking
-  //    when(snippetRepository.findById(anyLong())).thenReturn(Optional.of(snippet));
-  //    when(permissionService.deletePermissions(anyLong(), anyString()))
-  //        .thenReturn(FullResponse.create("Permission deleted", "permission", 1L, HttpStatus.OK));
-  //
-  //    // Test execution
-  //    ResponseEntity<ResponseDto<Long>> response = snippetService.deleteSnippet(1L, "user1");
-  //
-  //    // Assertions
-  //    assertEquals(HttpStatus.OK, response.getStatusCode());
-  //    assertEquals(snippetId, response.getBody().data().data());
-  //  }
+    @Test
+    public void testDeleteSnippet() {
+      Snippet snippet = new Snippet("Test Snippet", "user1", new Language("java", "1.8",
+   ".java"));
+      Long snippetId = 1L;
+
+      when(snippetRepository.findById(anyLong())).thenReturn(Optional.of(snippet));
+      when(permissionService.deletePermissions(1L, "user1"))
+          .thenReturn(FullResponse.create("Snippet deleted", "snippetId", 1L, HttpStatus.OK));
+
+      // Test execution
+      ResponseEntity<ResponseDto<Long>> response = snippetService.deleteSnippet(1L, "user1");
+
+      // Assertions
+      assertEquals(HttpStatus.OK, response.getStatusCode());
+      assertEquals(snippetId, response.getBody().data().data());
+    }
+
+
 }

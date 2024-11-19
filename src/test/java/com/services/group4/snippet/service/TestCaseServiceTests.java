985a984e89589348c745768e3a6aa6b9d1ec4017
JuliIglesias <jiglesias@mail.austral.edu.ar>
2024-11-19 15:48:49 -0300

    Tests for testCases 100%

--- src/test/java/com/services/group4/snippet/service/TestCaseServiceTests.java
+++ src/test/java/com/services/group4/snippet/service/TestCaseServiceTests.java
@@ -75,6 +75,27 @@ class TestCaseServiceTests {
   }
 
   @Test
+  void testCreateTestCase_InternalError() {
+    TestCaseRequestDto requestDto =
+        new TestCaseRequestDto(List.of("Input"), List.of("Output"), "Test Name");
+
+    when(permissionService.hasOwnershipPermission("user1", 1L))
+        .thenReturn(
+            FullResponse.create(
+                "Something went wrong creating the test case",
+                "permission",
+                false,
+                HttpStatus.INTERNAL_SERVER_ERROR));
+
+    ResponseEntity<ResponseDto<TestCaseResponseDto>> response =
+        testCaseService.createTestCase(requestDto, "user1", 1L);
+
+    assertEquals(HttpStatus.INTERNAL_SERVER_ERROR, response.getStatusCode());
+    assertNull(response.getBody().data().data());
+    assertEquals("Something went wrong creating the test case", response.getBody().message());
+  }
+
+  @Test
   void testGetTestCase_Success() {
     TestCase testCase1 =
         new TestCase("Test Name 1", 1L, List.of("Input 1"), List.of("Output 1"), TestState.PASSED);
@@ -120,6 +141,39 @@ class TestCaseServiceTests {
   }
 
   @Test
+  void testDeleteTestCase_Forbidden() {
+    TestCase testCase =
+        new TestCase("Test Name", 1L, List.of("Input"), List.of("Output"), TestState.NOT_STARTED);
+
+    when(testCaseRepository.findById(1L)).thenReturn(Optional.of(testCase));
+    when(permissionService.hasOwnershipPermission("user1", 1L))
+        .thenReturn(
+            FullResponse.create("Permission denied", "permission", false, HttpStatus.FORBIDDEN));
+
+    ResponseEntity<ResponseDto<String>> response = testCaseService.deleteTestCase("user1", 1L, 1L);
+
+    assertEquals(HttpStatus.FORBIDDEN, response.getStatusCode());
+    assertEquals("You don't have permission to delete the test case", response.getBody().message());
+  }
+
+  @Test
+  void testDeleteTestCase_InternalServerError() {
+    TestCase testCase =
+        new TestCase("Test Name", 1L, List.of("Input"), List.of("Output"), TestState.NOT_STARTED);
+
+    when(testCaseRepository.findById(1L)).thenReturn(Optional.of(testCase));
+    when(permissionService.hasOwnershipPermission("user1", 1L))
+        .thenReturn(
+            FullResponse.create(
+                "Permission denied", "permission", false, HttpStatus.INTERNAL_SERVER_ERROR));
+
+    ResponseEntity<ResponseDto<String>> response = testCaseService.deleteTestCase("user1", 1L, 1L);
+
+    assertEquals(HttpStatus.FORBIDDEN, response.getStatusCode());
+    assertEquals("You don't have permission to delete the test case", response.getBody().message());
+  }
+
+  @Test
   void testDeleteTestCase_NotFound() {
     when(testCaseRepository.findById(1L)).thenReturn(Optional.empty());
 
@@ -128,29 +182,86 @@ class TestCaseServiceTests {
     assertEquals(HttpStatus.NOT_FOUND, response.getStatusCode());
   }
 
-  // update case NO SABEMOS PORQUE NO FUNCIONA
-  //  @Test
-  //  void testUpdateTestCase_Success() {
-  //    TestCaseRequestDto requestDto =
-  //        new TestCaseRequestDto(List.of("Input"), List.of("Output"), "Test Name");
-  //    TestCase testCase =
-  //        new TestCase("Test Name", 1L, List.of("Input"), List.of("Output"),
-  // TestState.NOT_STARTED);
-  //
-  //    when(permissionService.hasOwnershipPermission("user1", 1L))
-  //        .thenReturn(FullResponse.create("Permission granted", "permission", true,
-  // HttpStatus.OK));
-  //    when(testCaseRepository.save(any(TestCase.class))).thenReturn(testCase);
-  //
-  //    // Ejecución
-  //    ResponseEntity<ResponseDto<TestCaseResponseStateDto>> response =
-  //        testCaseService.updateTestCase(requestDto, "user1", 1L, 1L);
-  //
-  //    // Validación
-  //    assertEquals(HttpStatus.OK, response.getStatusCode());
-  //    assertNotNull(response.getBody());
-  //    assertEquals("Test Name", response.getBody().data().data().name());
-  //  }
+  @Test
+  void testUpdateTestCase_Success() {
+    TestCaseRequestDto requestDto =
+        new TestCaseRequestDto(List.of("New Input"), List.of("New Output"), "Updated Name");
+
+    TestCase existingTestCase =
+        new TestCase(
+            "Old Name", 1L, List.of("Old Input"), List.of("Old Output"), TestState.NOT_STARTED);
+
+    TestCase updatedTestCase =
+        new TestCase(
+            "Updated Name", 1L, List.of("New Input"), List.of("New Output"), TestState.NOT_STARTED);
+
+    when(permissionService.hasOwnershipPermission("user1", 1L))
+        .thenReturn(FullResponse.create("Permission granted", "permission", true, HttpStatus.OK));
+    when(testCaseRepository.findById(1L)).thenReturn(Optional.of(existingTestCase));
+    when(testCaseRepository.save(any(TestCase.class))).thenReturn(updatedTestCase);
+
+    ResponseEntity<ResponseDto<TestCaseResponseStateDto>> response =
+        testCaseService.updateTestCase(requestDto, "user1", 1L, 1L);
+
+    assertEquals(HttpStatus.OK, response.getStatusCode());
+    assertNotNull(response.getBody());
+    assertEquals("Updated Name", response.getBody().data().data().name());
+    assertEquals(List.of("New Input"), response.getBody().data().data().inputs());
+    assertEquals(List.of("New Output"), response.getBody().data().data().outputs());
+  }
+
+  @Test
+  void testUpdateTestCase_PermissionDenied() {
+    TestCaseRequestDto requestDto =
+        new TestCaseRequestDto(List.of("New Input"), List.of("New Output"), "Updated Name");
+
+    TestCase existingTestCase =
+        new TestCase(
+            "Old Name", 1L, List.of("Old Input"), List.of("Old Output"), TestState.NOT_STARTED);
+
+    TestCase updatedTestCase =
+        new TestCase(
+            "Updated Name", 1L, List.of("New Input"), List.of("New Output"), TestState.NOT_STARTED);
+
+    when(permissionService.hasOwnershipPermission("user1", 1L))
+        .thenReturn(
+            FullResponse.create("Permission denied", "permission", false, HttpStatus.FORBIDDEN));
+    when(testCaseRepository.findById(1L)).thenReturn(Optional.of(existingTestCase));
+    when(testCaseRepository.save(any(TestCase.class))).thenReturn(updatedTestCase);
+
+    ResponseEntity<ResponseDto<TestCaseResponseStateDto>> response =
+        testCaseService.updateTestCase(requestDto, "user1", 1L, 1L);
+
+    assertEquals(HttpStatus.FORBIDDEN, response.getStatusCode());
+    assertNotNull(response.getBody());
+  }
+
+  @Test
+  void testUpdateTestCase_InternalServerError() {
+    TestCaseRequestDto requestDto =
+        new TestCaseRequestDto(List.of("New Input"), List.of("New Output"), "Updated Name");
+
+    TestCase existingTestCase =
+        new TestCase(
+            "Old Name", 1L, List.of("Old Input"), List.of("Old Output"), TestState.NOT_STARTED);
+
+    TestCase updatedTestCase =
+        new TestCase(
+            "Updated Name", 1L, List.of("New Input"), List.of("New Output"), TestState.NOT_STARTED);
+
+    when(permissionService.hasOwnershipPermission("user1", 1L))
+        .thenReturn(
+            FullResponse.create(
+                "Permission denied", "permission", false, HttpStatus.INTERNAL_SERVER_ERROR));
+    when(testCaseRepository.findById(1L)).thenReturn(Optional.of(existingTestCase));
+    when(testCaseRepository.save(any(TestCase.class))).thenReturn(updatedTestCase);
+
+    ResponseEntity<ResponseDto<TestCaseResponseStateDto>> response =
+        testCaseService.updateTestCase(requestDto, "user1", 1L, 1L);
+
+    assertEquals(HttpStatus.FORBIDDEN, response.getStatusCode());
+    assertNotNull(response.getBody());
+  }
 
   @Test
   void testUpdateTestCase_NotFound() {
